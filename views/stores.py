a353a4e002b42ff03f3e8be2e3a8abad0e38debe
german9304 <31298672+german9304@users.noreply.github.com>
2019-03-30 10:46:52 -0700

    templates nav bar

--- views/stores.py
+++ views/stores.py
@@ -5,8 +5,8 @@ from flask import (
 """Init flask blue print"""
 storesBp = Blueprint('stores', __name__)
 
-
 @storesBp.route('/')
+@storesBp.route('/stores/')
 def stores():
     """Route for stores."""
     return render_template('stores/stores.html')
