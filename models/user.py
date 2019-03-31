6bb3ee88b2f8813a604337aac9b79e1fdeb0c300
german9304 <31298672+german9304@users.noreply.github.com>
2019-03-31 16:26:59 -0700

    product, user models

--- models/user.py
+++ models/user.py
@@ -7,9 +7,13 @@ from .db import database
 
 model = database.get_db()
 
+
 class Users(model.Model):
+    """User model."""
     id = model.Column(model.Integer, primary_key=True)
-    email = model.Column(model.String(20), unique=True, nullable=False)
-    username = model.Column(model.String(20), unique=True, nullable=False)
+    email = model.Column(model.String(120), unique=True, nullable=False)
+    username = model.Column(model.String(120), unique=True, nullable=False)
+    products = model.relationship('Product', backref='users')
 
-
+    def get_products(self):
+        return self.products
