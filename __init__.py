71a29ccc380df8bd4cbd047a9bb1d35b086612cb
german9304 <31298672+german9304@users.noreply.github.com>
2019-03-31 10:18:31 -0700

    gitignore

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .views.main import homeBP
 from .views.stores import storesBp
-
+from .models.db import db
 
 def create_app():
     app = Flask(__name__)
+    db.init_db(app) # init database config
     app.register_blueprint(storesBp)
+
     return app
