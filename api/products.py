5558946e8918da9ed19b3050f1053ffb60ee69d4
german9304 <31298672+german9304@users.noreply.github.com>
2019-04-11 20:13:12 -0700

    create product review

--- api/products.py
+++ api/products.py
@@ -2,7 +2,13 @@ from flask import (
     request, views, redirect, jsonify, make_response
 )
 from shopcart.models import (
-    productSchema, userSchema, product, user
+    productSchema, 
+    userSchema, 
+    product, 
+    user, 
+    review, 
+    db,
+    reviewsSchema
 )
 from sqlalchemy import desc, exc
 import json
@@ -11,53 +17,47 @@ import functools
 def handler_404(func):
     @functools.wraps(func)
     def wrap_func(*params, **kwargs):
-        res = func(*params, **kwargs)
-        print(res)
-        return func(*params, **kwargs)
+        try:
+            res = func(*params, **kwargs)
+            return res
+        except (exc.SQLAlchemyError, Exception) as e:
+            # print(str(e))
+            resp = {'success': False, 'data': str(e)}
+            header = {'content-type': 'application/json'}
+            res = (json.dumps(resp), 404, header)
+            return make_response(res)
+
     return wrap_func
 
 
 class ProductsAPI(views.MethodView):
     """Products RESTful API."""
 
-    def get_product(self, product_id):
-        try:
-            resp = product.Product.query.get(product_id)
-        except (exc.SQLAlchemyError, Exception) as e:
-            print(e)
-            # return make_response()
-            return jsonify({'user':'user'})
-        # print(resp)
-        return resp
-    
-    # @handler_404
+    @handler_404
     def get(self, product_id=None):
         """HTTP GET method."""
 
         if product_id:
-            print(product_id)
-            data = self.get_product(product_id)
-            if data:
-                res = productSchema.PRODUCT_SCHEMA \
-                        .dump(data)
-                # print(type(data))
-                return jsonify(data=res.data, errors=res.errors)
-            else:
-                data = {'success':False, 'data':'user not found'}
-                header = {'content-type': 'application/json'}
-                res = (json.dumps(data), 404, header)
-                return make_response(res)
+            product_data = product.Product.query \
+                .filter(product.Product.id == product_id).first_or_404()
+            serialize_product = productSchema.PRODUCT_SCHEMA \
+                    .dump(product_data)
+            return jsonify(data=serialize_product.data)
 
-        products = product.Product.query.all()
-        data = productSchema.PRODUCTS_SCHEMA.dump(products)
-        return jsonify(products=data)
+        products_data = product.Product.query.all()
+        serialize_products = productSchema.PRODUCTS_SCHEMA.dump(products_data)
+        return jsonify(data=serialize_products)
 
+    @handler_404
     def post(self, product_id):
         """HTTP POST method."""
-        res = request.json
-        print(res)
-        
-        return jsonify(name='john', age=32)
+        res = request.json['comment']
+        user_id = 2
+        rev = review.Reviews(product_id=product_id, user_id=user_id, 
+                comment=res)
+        review_data = db.database.insert(rev)
+        serialize_review = reviewsSchema.REVIEW_SCHEMA.dump(review_data)
+        return jsonify(data=serialize_review)
 
     def put(self):
         """HTTP PUT METHOD."""
@@ -75,3 +75,18 @@ def register_product_api(app, endpoint, url='/api/products'):
                     methods=['GET'])
     app.add_url_rule('/api/product/<int:product_id>/', view_func=view_func, 
                     methods=['POST', 'PUT', 'DELETE', 'GET'])
+
+
+
+#     try:
+#         product_data = product.Product.query. \
+#             filter(product.Product.id == product_id).one()
+#         res = productSchema.PRODUCT_SCHEMA \
+#                 .dump(product_data)
+#         return jsonify(data=res.data, errors=res.errors)
+#     except (exc.SQLAlchemyError, Exception) as e:
+#         # print(type(e))
+#         resp = {'success':False, 'data': ''}
+#         header = {'content-type': 'application/json'}
+#         res = (json.dumps(resp), 404, header)
+#         return make_response(res)
